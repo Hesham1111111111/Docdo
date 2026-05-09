4cbb7df0e4c7d2a16c275f11e2c8a0e41990f93e
Hesham <eidhesham999@gmail.com>
2026-05-09 15:26:11 +0300

    Added doctor speciality details screen

--- lib/features/home/presention/views/widget/doctor_speciality/doctor_speciality_seeAll.dart
+++ lib/features/home/presention/views/widget/doctor_speciality/doctor_speciality_seeAll.dart
@@ -1,5 +1,8 @@
+import 'package:advanced/core/helpers/exetions.dart';
 import 'package:advanced/core/theming/app_stayle.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../../../core/routing/rouets.dart';
 
 class DoctorSpecialitySeeAll extends StatelessWidget {
   const DoctorSpecialitySeeAll({super.key});
@@ -11,7 +14,9 @@ class DoctorSpecialitySeeAll extends StatelessWidget {
         Text('Doctor Speciality', style: AppStyle.text18BlackBold),
        const  Spacer(),
         InkWell(
-          onTap: () {},
+          onTap: () {
+            context.pushNamed(Routes.doctorSpecialitySeeAll);
+          },
 
           child: Text("See All ", style: AppStyle.text14PrimaryBold),
         ),
---
+++ lib/features/home/presention/views/widget/doctor_speciality/doctor_speciality_see_all_screen.dart
@@ -0,0 +1,39 @@
+import 'package:advanced/features/home/presention/manager/home_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../../../core/di/service_locator.dart';
+import 'doctor_specialty_bolc_.dart';
+
+
+class DoctorSpecialitySeeAllScreen extends StatelessWidget {
+  const DoctorSpecialitySeeAllScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => getIt<HomeCubit>()..getDoctorSpecialities(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Doctor Specialities'),
+        ),
+        body: GridView.builder(
+          padding: const EdgeInsets.all(16),
+          itemCount: 10,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 12,
+            mainAxisSpacing: 12,
+            childAspectRatio: 1,
+          ),
+          itemBuilder: (context, index) {
+            return Card(
+              child: Center(
+                child: DoctorSpecialtyBloc(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
